57393a2e8c7732b8a919695bb1a4847fe4f45241
milandiaz <milandiaz2000@hotmail.com>
2025-06-25 12:21:48 -0500

    Added features and made further changes

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ function addNumber(number) {
 
 function interface() {
   const form = document.createElement("form");
+
+  const message = document.createElement("label");
+  message.textContent = "Add a number tot he bank:";
+  form.appendChild(message);
+
   const input = document.createElement("input");
-  input.textContent = "Add a number tot he bank:";
+  input.type = "number";
   form.appendChild(input);
+
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add number";
+  form.appendChild(addButton);
+
+  const sortOne = document.createElement("button");
+  sortOne.textContent = "Sort 1";
+  form.appendChild(sortOne);
+
+  const sortAll = document.createElement("button");
+  sortAll.textContent = "Sort All";
+  form.appendChild(sortAll);
 
   return form;
 }
