2ce24f355518cb995872054a2b6f078bbbcf8293
milandiaz <milandiaz2000@hotmail.com>
2025-06-25 12:43:54 -0500

    Further changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function interface() {
   const form = document.createElement("form");
 
   const message = document.createElement("label");
-  message.textContent = "Add a number tot he bank:";
+  message.textContent = "Add a number to the bank:";
   form.appendChild(message);
 
   const input = document.createElement("input");
@@ -22,18 +22,41 @@ function interface() {
 
   const addButton = document.createElement("button");
   addButton.textContent = "Add number";
+  addButton.addEventListener("click", () => {
+    addNumber();
+    render();
+  });
   form.appendChild(addButton);
 
   const sortOne = document.createElement("button");
   sortOne.textContent = "Sort 1";
+  sortOne.addEventListener("click", () => {
+    sortOneNumber();
+    render();
+  });
   form.appendChild(sortOne);
 
   const sortAll = document.createElement("button");
   sortAll.textContent = "Sort All";
+  sortAll.addEventListener("click", () => {
+    sortAllNumbers();
+    render();
+  });
   form.appendChild(sortAll);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = input.value;
+    addNumber(value);
+    input.value = "";
+  });
 
   return form;
 }
+
+function sortOneNumber() {}
+
+function sortAllNumbers() {}
 
 function render() {
   const $app = document.querySelector("#app");
